0b270d22b03c11951782df094dd983fbeb8027cf
Gabriel Contarini <gcontari@c1r17s7.42lisboa.com>
2021-12-17 15:17:56 +0000

    Fix stk reverse rotate

--- stk/ft_rrot_istack.c
+++ stk/ft_rrot_istack.c
@@ -6,7 +6,7 @@
 /*   By: gcontari <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/16 10:19:12 by gcontari          #+#    #+#             */
-/*   Updated: 2021/12/16 10:19:52 by gcontari         ###   ########.fr       */
+/*   Updated: 2021/12/17 15:17:23 by gcontari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,17 @@
 void	ft_rrot_istack(t_istack *stack)
 {
 	t_isunit	*top;
-	t_isunit	*base;
+	t_isunit	*curr;
 
 	if (!stack || !stack->top || !stack->base || !stack->top->last)
 		return ;
 	top = stack->top;
+	curr = top;
+	while (curr->last->last)
+		curr = curr->last;
+	curr->last = NULL;
 	stack->top = stack->base;
-	base = stack->base;
-	base->last = top;
+	stack->base->last = top;
+	stack->base = curr;
 	return ;
 }
