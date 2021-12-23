8129493723c02875177e54407f5efbd1a9b144c0
Gabriel Contarini <gcontari@c1r17s7.42lisboa.com>
2021-12-23 17:06:33 +0000

    Change get_next_line and drop the use of gnl utilities file

--- gnl/get_next_line.c
+++ gnl/get_next_line.c
@@ -6,93 +6,87 @@
 /*   By: gcontari <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/18 15:16:37 by gcontari          #+#    #+#             */
-/*   Updated: 2021/11/06 14:10:31 by gcontari         ###   ########.fr       */
+/*   Updated: 2021/12/23 17:04:30 by gcontari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static char		*cpybuff_nl(char *buff);
-static char		*strcat_nl(char *dst, char *src);
-static void		upbuff_nl(char *buff);
-static size_t	strlen_nl(char *str);
+static char		*gnl_strjoin_char(char *dst, const char *src, char c);
+static char		*gnl_newline(char *buff, char c);
+static void		gnl_upbuff_char(char *buff, char c);
+static size_t	gnl_strlen_char(const char *str, char c);
 
 char	*get_next_line(int fd)
 {
 	static char	buff[BUFFER_SIZE + 1];
-	long		rc;
-	char		*p;
+	ssize_t		rc;
+	char		*line;
 
 	if (fd < 0 || fd > MAX_FD || BUFFER_SIZE < 1)
 		return (NULL);
-	p = cpybuff_nl(buff);
-	if (check_strchr(p, '\n'))
-		return (p);
+	line = gnl_newline(buff, '\n');
+	if (ft_strchr(line, '\n'))
+		return (line);
 	rc = read(fd, buff, BUFFER_SIZE);
 	while (rc > 0)
 	{
 		buff[rc] = 0;
-		p = strcat_nl(p, buff);
-		if (!p || check_strchr(buff, '\n'))
+		line = gnl_strjoin_char(line, buff, '\n');
+		if (!line || ft_strchr(buff, '\n'))
 			break ;
 		rc = read(fd, buff, BUFFER_SIZE);
 	}
-	if (p)
-		upbuff_nl(buff);
-	return (p);
+	if (line)
+		gnl_upbuff_char(buff, '\n');
+	return (line);
 }
 
-static char	*cpybuff_nl(char *buff)
+static char	*gnl_newline(char *buff, char c)
 {
+	char	*line;
+
+	line = 0;
+	line = gnl_strjoin_char(line, buff, c);
+	if (!line)
+		return (NULL);
+	gnl_upbuff_char(buff, '\n');
+	return (line);
+}
+
+static char	*gnl_strjoin_char(char *dst, const char *src, char c)
+{
+	size_t	i;
 	size_t	len;
 	char	*p;
 
-	len = strlen_nl(buff);
+	len = ft_strlen(dst) + gnl_strlen_char(src, c);
 	if (len == 0)
 		return (NULL);
 	p = (char *) malloc((len + 1) * sizeof(char));
 	if (!p)
 		return (NULL);
-	while (*buff && *buff != '\n')
-		*p++ = *buff++;
-	if (*buff == '\n')
-		*p++ = *buff++;
-	*p = 0;
-	upbuff_nl(buff - len);
-	return (p - len);
-}
-
-static char	*strcat_nl(char *s1, char *s2)
-{
-	size_t	i;
-	size_t	len;
-	char	*p;
-
-	len = ft_strlen(s1) + strlen_nl(s2);
-	p = (char *) malloc((len + 1) * sizeof(char));
-	if (!p)
-		return (NULL);
 	i = 0;
-	while (s1 && s1[i])
-		*p++ = s1[i++];
-	while (*s2 && *s2 != '\n')
-		*p++ = *s2++;
-	if (*s2 == '\n')
-		*p++ = '\n';
+	while (dst && dst[i])
+		*p++ = dst[i++];
+	while (*src && *src != c)
+		*p++ = *src++;
+	if (*src == c)
+		*p++ = c;
 	*p = 0;
-	if (s1)
-		free(s1);
+	if (dst)
+		free(dst);
 	return (p - len);
 }
 
-static void	upbuff_nl(char *buff)
+static void	gnl_upbuff_char(char *buff, char c)
 {
 	size_t	i;
 
 	i = 0;
-	while (buff[i] && buff[i] != '\n')
+	while (buff[i] && buff[i] != c)
 		i++;
-	if (buff[i] == '\n')
+	if (buff[i] == c)
 		i++;
 	while (buff[i])
 	{
@@ -102,14 +96,16 @@ static void	upbuff_nl(char *buff)
 	*buff = 0;
 }
 
-static size_t	strlen_nl(char *str)
+static size_t	gnl_strlen_char(const char *str, char c)
 {
 	size_t	len;
 
+	if (!str)
+		return (0);
 	len = 0;
-	while (str && str[len] && str[len] != '\n')
+	while (str[len] && str[len] != c)
 		len++;
-	if (str && str[len] == '\n')
+	if (str[len] == c)
 		len++;
 	return (len);
 }
--- includes/get_next_line.h
+++ includes/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: gcontari <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/18 14:34:42 by gcontari          #+#    #+#             */
-/*   Updated: 2021/11/30 17:09:08 by gcontari         ###   ########.fr       */
+/*   Updated: 2021/12/23 16:00:45 by gcontari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,13 @@
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 1024
 # endif
-# define MAX_FD 1024
+# ifndef MAX_FD
+#  define MAX_FD 1024
+# endif
 # include <unistd.h>
 # include <stdlib.h>
 # include "libft.h"
 
 char	*get_next_line(int fd);
-size_t	check_strchr(char *p, char c);
 
 #endif
